cf992e1f034d979207685692e06c8bf83d6ff92e
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-14 03:07:40 +0530

    Learnt how to append and rename a file

--- node-basics/script.js
+++ node-basics/script.js
@@ -16,3 +16,23 @@ fs.writeFile("test.txt", "My first node js code", function(err) {
         console.log("Done")
     }
 })
+
+
+// Append something to the old file
+fs.appendFile("test.txt", "My second line of node js code", function(err) {
+    if(err) {
+        console.error(err)
+    } else {
+        console.log("Done")
+    }
+})
+
+// Rename the file
+
+fs.rename("test.txt", "helloWorld.txt", function(err) {
+    if(err) {
+        console.error(err)
+    } else {
+        console.log("Done")
+    }
+})
