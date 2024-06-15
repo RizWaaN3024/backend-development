9ac7de05ff358fb7e31ad448832d0262ec272863
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-16 01:16:06 +0530

    EJS setup

--- basic-practice-project/index.js
+++ basic-practice-project/index.js
@@ -4,9 +4,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
-    res.send("Its Running");
+    res.render("index");
 })
 
 app.listen(3000, () => {
