bac430b95e89d272778964aed92e7a4c20cd282e
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-16 01:32:51 +0530

    Set up dynamic routing

--- basic-practice-project/index.js
+++ basic-practice-project/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
     res.render("index");
+})
+// Dynamic Routing
+app.get("/profile/:username", (req, res) => {
+    // req.params.username
+    res.send("Running")
 })
 
 app.listen(3000, () => {
