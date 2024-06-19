c9b4d22b0db422e0155e8510dbd2adf2eaca2c07
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-20 01:51:15 +0530

    Added basic crud methods

--- mongo-db-basics/app.js
+++ mongo-db-basics/app.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const app = express();
 
+const userModel = require("./usermodel");
+const usermodel = require("./usermodel");
 
 app.get("/", (req, res) => {
     res.send("Hello")
 })
 
+app.get("/create", async (req, res) => {
+    let createdUser = await userModel.create({
+        name: "Rizwaan",
+        email: "rizwaanans786@gmail.com",
+        username: "rizwaan"
+    })
+    res.send(createdUser)
+})
+// update
+app.get("/update", async (req, res) => {
+    let updatedUser = await usermodel.findOneAndUpdate({username: "rizwaan"}, {name: "Rizwaan Ansari"}, {new: true})
+    res.send(updatedUser)
+})
+
+// read
+app.get("/read", async (req, res) => {
+    let users = await usermodel.find();
+    res.send(users)
+})
+// delete
+app.get("/delete", async (req, res) => {
+    let deletedUser = await userModel.findOneAndDelete({username: "rizwaan"})
+    res.send(deletedUser)
+})
 app.listen(3000)
