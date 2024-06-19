bc1fcd75193b2671f8771d1e34fe19bff261dfb5
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-19 21:12:29 +0530

    Created User model

---
+++ mongo-db-basics/usermodel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(`mongodb://127.0.0.1:27027/mongopractice`);
+
+const userSchema = mongoose.Schema({
+    name: String,
+    username: String,
+    email: String
+})
+
+module.exports = mongoose.model("user", userSchema);
