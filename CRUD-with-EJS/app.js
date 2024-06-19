8b82e1aa24fd72af6d6ebeebcbb6ea7f0478883a
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-20 02:09:25 +0530

    Initial setup

---
+++ CRUD-with-EJS/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const path = require("path")
+
+app.set("view engine", "ejs")
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.listen(3000)
