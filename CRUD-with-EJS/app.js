cd56d4ac3c78ed848044731f05aaf02f4a0ae33d
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-21 01:38:48 +0530

    Added post read and delete methods

--- CRUD-with-EJS/app.js
+++ CRUD-with-EJS/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path")
+const userModel = require("./models/user")
 
 app.set("view engine", "ejs")
 app.use(express.json());
@@ -13,7 +14,26 @@ app.get("/", (req, res) => {
 })
 
 // read route
-app.get('/read', (req, res) => {
-    res.render("read")
+app.get('/read', async (req, res) => {
+    let allUser = await userModel.find();
+
+    res.render("read", {allUser})
+})
+// post route
+app.post("/create", async (req, res) => {
+    let {name, email, image} = req.body;
+    let createdUser = await userModel.create({
+        name,
+        email,
+        image
+    })
+
+    res.redirect("/read")
+})
+
+// Delete User
+app.get("/delete/:id", async (req, res) => {
+    let user = await userModel.findOneAndDelete({_id: req.params.id})
+    res.redirect("/read")
 })
 app.listen(3000)
