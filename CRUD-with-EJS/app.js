265ddfde530d8dc5bff365b79fd63f60e23e4212
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-21 01:52:53 +0530

    Added Update Method

--- CRUD-with-EJS/app.js
+++ CRUD-with-EJS/app.js
@@ -36,4 +36,17 @@ app.get("/delete/:id", async (req, res) => {
     let user = await userModel.findOneAndDelete({_id: req.params.id})
     res.redirect("/read")
 })
+
+// edit user
+app.get("/edit/:userid", async (req, res) => {
+    let user = await userModel.findOne({_id: req.params.userid})
+    res.render("edit", {user})
+})
+
+app.post("/update/:userid", async (req, res) => {
+    let {image, name, email} = req.body;
+    let user = await userModel.findOneAndUpdate({_id: req.params.userid}, {image, name, email}, {new: true})
+    res.redirect("/read")
+})
+
 app.listen(3000)
