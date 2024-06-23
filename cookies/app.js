82d3fc4c753ed51e03bff9f2fb3c5d4f35bf4dd5
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-24 02:40:19 +0530

    Learnt Bcrypt

--- cookies/app.js
+++ cookies/app.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const app = express();
 const cookieParser = require('cookie-parser')
+const bcrypt = require("bcrypt");
 
 
 app.use(cookieParser())
 
 
 app.get("/", (req, res) => {
-    res.cookie("name", "Rizwaan")
-    res.send("Done")
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash("myPlainTextPassword", salt, function(err, hash) {
+            // Store hash in your password DB
+            console.log(hash)
+        })
+    })
+    res.send("Working")
 })
 app.get("/read", (req, res) => {
     console.log(req.cookies)
