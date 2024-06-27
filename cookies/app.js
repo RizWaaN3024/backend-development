16263bb34e424f93152e4f774a8e0f284e5531cd
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-28 01:22:26 +0530

    Learnt JWT

--- cookies/app.js
+++ cookies/app.js
@@ -3,6 +3,9 @@ const app = express();
 const cookieParser = require('cookie-parser')
 const bcrypt = require("bcrypt");
 
+// JWT
+var jwt = require('jsonwebtoken')
+
 
 app.use(cookieParser())
 
@@ -11,14 +14,18 @@ app.get("/", (req, res) => {
     bcrypt.genSalt(10, function(err, salt) {
         bcrypt.hash("myPlainTextPassword", salt, function(err, hash) {
             // Store hash in your password DB
-            console.log(hash)
+            // console.log(hash)
         })
     })
+
+    let token = jwt.sign({email: "rizwaanans786@gmail.com"}, "secret")
+    res.cookie("token", token)
+    console.log(token)
     res.send("Working")
 })
 app.get("/read", (req, res) => {
-    console.log(req.cookies)
-    res.send("READ PAGE")
+    let data = jwt.verify(req.cookies.token, "secret")
+    console.log(data)
 })
 
 app.listen(3000)
