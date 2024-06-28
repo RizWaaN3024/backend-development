b30495794209b5b4c53bf81e2ac45f788b9fd5a6
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-29 01:59:59 +0530

    Created User

--- data-association/app.js
+++ data-association/app.js
@@ -1,8 +1,19 @@
 const express = require("express")
 const app = express()
+const userModel = require("./models/user")
+const postModel = require("./models/post")
 
 app.get("/", (req, res) => {
     res.send("Hello")
+})
+
+app.get("/create", async (req, res) => {
+    let user = await userModel.create({
+        username: "Rizwaan",
+        age: "21",
+        email: "rizwaanans786@gmail.com"
+    })
+    res.send(user)
 })
 
 app.listen(3000)
